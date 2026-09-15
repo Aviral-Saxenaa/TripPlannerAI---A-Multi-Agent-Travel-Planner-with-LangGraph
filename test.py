ddca0e7958708e303de20a1eb5e9fddb47917062
Aviral Saxena <aviral.saxena.11.70@gmail.com>
2026-09-15 22:52:10 +0530

    aviation local mcp + custom local weather mcp

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import asyncio
-from mcp_client_test import get_all_tools, tavily_mcp_search
-# from mcp_client_test import get_all_tools
+# from mcp_client_test import get_all_tools, tavily_mcp_search
+from mcp_client import get_all_tools
 
 
 
 if __name__ == "__main__":
-    query="last football world cup match played"
-    asyncio.run(tavily_mcp_search(query))
+    # query="last football world cup match played"
+    asyncio.run(get_all_tools())
